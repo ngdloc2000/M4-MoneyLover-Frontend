9e1dfbbbb1a60cefa546b6fb7bd9dad00fb3c42b
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-12-18 07:44:07 +0700

    wallet by user

--- javascript/user/home.js
+++ javascript/user/home.js
@@ -1,1 +1,34 @@
 let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/info/" + currentUser.id,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function (data) {
+            let avatar = `<img style="border-radius: 50%; height: 40px; width: 40px" src="../../img/${data.avatar}">`;
+            let name = `${data.name}`;
+            document.getElementById('avatar-img').innerHTML = avatar;
+            document.getElementById('name-user').innerHTML = name;
+        }
+    })
+}
+
+function getWalletByUser() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/wallets/walletByUserId/" + currentUser.id,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function (data) {
+            let balance = `${data.balance} <span>đ</span>`;
+            document.getElementById('balace-wallet').innerHTML = balance;
+        }
+    })
+}
+
+getWalletByUser();
+getUserInfo();
