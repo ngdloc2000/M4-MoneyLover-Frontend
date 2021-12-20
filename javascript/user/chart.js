b830bc108316833219de68a35475e87e1405bf00
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-12-20 23:33:22 +0700

    input form get sum amount each day in month

--- javascript/user/chart.js
+++ javascript/user/chart.js
@@ -1,46 +1,69 @@
-let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+let currentUser= JSON.parse(localStorage.getItem("currentUser"));
 
-// $.ajax({
-//     type: "GET",
-//     url: "http://localhost:8080/transactions/sumAmountEachDayInMonth/1/1/12",
-//     headers: {
-//         'Authorization': 'Bearer ' + currentUser.token
-//     },
-//     success: function (data) {
-//         var dataPoints = [];
-//         var chart;
-//         $.each(data, function (key, value) {
-//             dataPoints.push({x: value.day, y: parseInt(value.amount)});
+// window.onload = function () {
+//     var dataPoints = [];
+//     var chart;
+//     $.ajaxSetup({
+//         headers: {
+//             'Authorization': 'Bearer ' + currentUser1.token
+//         }
+//     });
+//     $.getJSON("http://localhost:8080/transactions/sumAmountEachDayInMonth/1/1/12",
+//         function (data) {
+//             $.each(data, function (key, value) {
+//                 dataPoints.push({y: parseInt(value.amount), label: parseInt(value.day)});
+//             });
+//             chart = new CanvasJS.Chart("chartContainer", {
+//                 title: {
+//                     text: "SỐ LƯỢNG TIỀN CHI RA MỖI NGÀY TRONG THÁNG"
+//                 },
+//                 data: [{
+//                     type: "column",
+//                     dataPoints: dataPoints,
+//                 }]
+//             });
+//             chart.render();
 //         });
-//         chart = new CanvasJS.Chart("chartContainer", {
-//             title: {
-//                 text: "Số tiền chi ra mỗi ngày trong tháng"
-//             },
-//             data: [{
-//                 type: "column",
-//                 dataPoints: dataPoints,
-//             }]
-//         });
-//         chart.render();
-//     }
-// })
+// }
 
-window.onload = function () {
-    var dataPoints = [];
-    var chart;
+function getAllCategories() {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/categories`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function (data) {
+            let content = "";
+            for (let i = 0; i < data.length; i++) {
+                content +=
+                    `
+                        <option value="${data[i].id}">${data[i].name}</option>
+                    `
+            }
+            document.getElementById('cateSelected').innerHTML = content;
+        }
+    });
+}
+
+function sumAmount () {
+    let cateId = $('#cateSelected').val();
+    let month = $('#month').val();
+    let dataPoints = [];
+    let chart;
     $.ajaxSetup({
         headers: {
             'Authorization': 'Bearer ' + currentUser.token
         }
     });
-    $.getJSON("http://localhost:8080/transactions/sumAmountEachDayInMonth/1/1/12",
+    $.getJSON("http://localhost:8080/transactions/sumAmountEachDayInMonth/" + cateId + "/" + currentUser.id + "/" + month,
         function (data) {
             $.each(data, function (key, value) {
                 dataPoints.push({y: parseInt(value.amount), label: parseInt(value.day)});
             });
             chart = new CanvasJS.Chart("chartContainer", {
                 title: {
-                    text: "Live Chart with dataPoints from External JSON"
+                    text: "SỐ LƯỢNG TIỀN CHI RA MỖI NGÀY TRONG THÁNG"
                 },
                 data: [{
                     type: "column",
@@ -50,3 +73,5 @@ window.onload = function () {
             chart.render();
         });
 }
+
+getAllCategories();
