cd216f5eda6dc3c24950b84d91c9a431c146dc9e
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-12-21 08:21:07 +0700

    bieu do

---
+++ javascript/user/chart.js
@@ -0,0 +1,51 @@
+let currentUser= JSON.parse(localStorage.getItem("currentUser"));
+
+function getAllCategories() {
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/categories`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function (data) {
+            let content = "";
+            for (let i = 0; i < data.length; i++) {
+                content +=
+                    `
+                        <option value="${data[i].id}">${data[i].name}</option>
+                    `
+            }
+            document.getElementById('cateSelected').innerHTML = content;
+        }
+    });
+}
+
+function sumAmount () {
+    let cateId = $('#cateSelected').val();
+    let month = $('#month').val();
+    let dataPoints = [];
+    let chart;
+    $.ajaxSetup({
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        }
+    });
+    $.getJSON("http://localhost:8080/transactions/sumAmountEachDayInMonth/" + cateId + "/" + currentUser.id + "/" + month,
+        function (data) {
+            $.each(data, function (key, value) {
+                dataPoints.push({y: parseInt(value.amount), label: parseInt(value.day)});
+            });
+            chart = new CanvasJS.Chart("chartContainer", {
+                title: {
+                    text: "SỐ LƯỢNG TIỀN CHI RA MỖI NGÀY TRONG THÁNG"
+                },
+                data: [{
+                    type: "column",
+                    dataPoints: dataPoints,
+                }]
+            });
+            chart.render();
+        });
+}
+
+getAllCategories();
